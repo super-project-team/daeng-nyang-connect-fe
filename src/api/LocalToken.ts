2739115ff3750c9e25c6c05012d7972ee888fe47
joeyoungsang <127814297+joeyoungsang@users.noreply.github.com>
2024-01-04 15:43:47 +0900

    fix: api 주소 변경
    fix: api 주소 변경

--- src/api/LocalToken.ts
+++ src/api/LocalToken.ts
@@ -1,19 +1,19 @@
 class LocalToken {
-  #TOKEN_KEY = 'access_token'
+	#TOKEN_KEY = 'access_token';
 
-  save(token: string) {
-    localStorage.setItem(this.#TOKEN_KEY, token)
-  }
+	save(token: string) {
+		localStorage.setItem(this.#TOKEN_KEY, token);
+	}
 
-  get() {
-    return localStorage.getItem(this.#TOKEN_KEY)
-  }
+	get() {
+		return localStorage.getItem(this.#TOKEN_KEY);
+	}
 
-  remove() {
-    localStorage.removeItem(this.#TOKEN_KEY)
-  }
+	remove() {
+		localStorage.removeItem(this.#TOKEN_KEY);
+	}
 }
 
-const localToken = new LocalToken()
+const localToken = new LocalToken();
 
-export default localToken
+export default localToken;
